cd7337caa94510aa9f1a168d5f881ee1e757a9cc
mabiak <mabiane.polniak@gmail.com>
2022-06-01 17:51:30 -0300

    feat: create models and migrations

---
+++ app/backend/src/database/models/match.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class match extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  match.init({
+    home_team: DataTypes.INTEGER,
+    home_team_goals: DataTypes.INTEGER,
+    away_team: DataTypes.INTEGER,
+    away_team_goals: DataTypes.INTEGER,
+    in_progress: DataTypes.BOOLEAN
+  }, {
+    timestamps: false,
+    sequelize,
+    modelName: 'match',
+  });
+  return match;
+};
