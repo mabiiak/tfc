8272e48eed0ad66c2496f1fb635e848d0fcccc97
mabiiak <mabiane.polniak@gmail.com>
2022-06-26 23:32:32 -0300

    test: create test route leaderboard

---
+++ app/backend/src/tests/leader.test.ts
@@ -0,0 +1,40 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+import { Response } from 'superagent';
+
+import Teams from '../database/models/team';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('4 - Testa rota: GET/teams', () => {
+  let chaiHttpResponse: Response;
+
+  it('Retorna todos os times', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/leaderboard/home');
+    
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).length(16);
+  });
+});
+
+describe('5 - Testa rota: GET/teams', () => {
+  let chaiHttpResponse: Response;
+
+  it('Retorna todos os times', async () => {
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/leaderboard/away');
+    
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).length(16);
+  });
+});
